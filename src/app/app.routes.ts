7f6099f0c9afb57a85a7c8099fe46339e97011ab
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-02-27 10:15:23 -0500

    feat: 404 page & more

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: HomeComponent
+    },
+    {
+        path: '**', component: NotFoundComponent
+    }
+];
